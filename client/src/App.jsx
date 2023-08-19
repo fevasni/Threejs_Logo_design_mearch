2f3a69798e3118539708e2d6324376ca448e1fef
feveasni <fevenaanak3624@gmail.com>
2023-08-19 11:19:12 -0700

    first-commit

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import  Canvas from './canvas'
+import Customizer from './pages/Customizer'
+import Home from './pages/Home'
+
+
+function App() {
+
+  return (
+    <main className='app transtion-all ease-in'>
+      <Home />
+      <Canvas />
+      <Customizer />
+    </main>
+  )
+}
+
+export default App
